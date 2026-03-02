cc70b3a33bd354b1d1a5b80a705f05d250ff0996
ipavlopoulos <annis@aueb.gr>
2026-03-02 10:54:20 +0200

    Added code for class building (Human) and precision loss (Variables).

---
+++ gr/aueb/cs/projects/introduction/Human.java
@@ -0,0 +1,43 @@
+package gr.aueb.cs.projects.introduction;
+
+class Human
+{
+    boolean alive;
+    int age=0;
+    private String name;
+
+
+    public void setName(String name){
+        if (this.name == null) {
+            this.name = name;
+        }else{
+            System.out.println("Already set");
+        }
+    }
+
+    private void born()
+    {
+        alive = true;
+    }
+
+    public void speak()
+    {
+        System.out.println("Hello world!");
+    }
+
+    void incrementAge()
+    {
+        age += 1;
+    }
+
+    public static void main(String[] args){
+        char c='B';
+        System.out.println(c+1);
+        Human h = new Human(); // new object
+        h.setName("John");
+        h.setName("Nobody");
+        h.name = "";
+        System.out.println(h.name); // print a field
+        h.speak();
+    }
+}
---
+++ gr/aueb/cs/projects/introduction/Variables.java
@@ -0,0 +1,53 @@
+package gr.aueb.cs.projects.introduction;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        // overflow
+        byte x = 127;
+
+        System.out.println(x);
+        x += 1;
+        System.out.println(x);
+
+        // precision loss
+        double d = 3.14;
+        float f = 3.14f;
+        System.out.println(d);
+        System.out.println(f);
+
+        double big = 1.123456789012345;
+        float small = 1.123456789012345f;
+        System.out.println(big);
+        System.out.println(small);
+
+        /* ⚠️
+            Όταν γράφουμε δεκαδικό αριθμό, η Java υποθέτει
+            ότι θέλουμε τον πιο *ακριβή* τύπο, δηλαδή double.
+            Αν θέλουμε float, πρέπει να το δηλώσουμε.
+         */
+        precisionLoss();
+    }
+
+    static void precisionLoss(){
+        // Ο αριθμός 16.777.217 χρειάζεται 25 bits για να αναπαρασταθεί πλήρως
+        int originalInt = 16777217;
+
+        // Αυτόματη μετατροπή (widening primitive conversion) από int σε float
+        float promotedFloat = originalInt;
+
+        // Μετατροπή ξανά σε int για να δούμε αν χάθηκε κάτι
+        int backToInt = (int) promotedFloat;
+
+        System.out.println("Αρχικός Int:  " + originalInt);
+        System.out.println("Τιμή σε Float: " + promotedFloat);
+        System.out.println("Int μετά τη μετατροπή: " + backToInt);
+
+        if (originalInt != backToInt) {
+            System.out.println("\nΠΡΟΣΟΧΗ: Οι τιμές ΔΕΝ είναι ίσες!");
+            System.out.println("Διαφορά: " + (originalInt - backToInt));
+        }
+    }
+
+
+}
