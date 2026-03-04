b8b5f03fae77930a87af7f327270a887427b3f37
ipavlopoulos <annis@aueb.gr>
2026-03-04 19:29:14 +0200

    Code added in the introductory project (projects > introduction)

--- gr/aueb/cs/projects/introduction/Variables.java
+++ gr/aueb/cs/projects/introduction/Variables.java
@@ -2,7 +2,40 @@ package gr.aueb.cs.projects.introduction;
 
 public class Variables {
 
+    private int x;
+
+    public Variables(){
+        this.x = 0;
+    }
+
+    private void precisionLoss(){
+        // Ο αριθμός 16.777.217 χρειάζεται 25 bits για να αναπαρασταθεί πλήρως
+        int originalInt = 16777217;
+
+        // Αυτόματη μετατροπή (widening primitive conversion) από int σε float
+        float promotedFloat = originalInt;
+
+        // Μετατροπή ξανά σε int για να δούμε αν χάθηκε κάτι
+        int backToInt = (int) promotedFloat;
+
+        System.out.println("Αρχικός Int:  " + originalInt);
+        System.out.println("Τιμή σε Float: " + promotedFloat);
+        System.out.println("Int μετά τη μετατροπή: " + backToInt);
+
+        if (originalInt != backToInt) {
+            System.out.println("\nΠΡΟΣΟΧΗ: Οι τιμές ΔΕΝ είναι ίσες!");
+            System.out.println("Διαφορά: " + (originalInt - backToInt));
+        }
+
+    }
+
     public static void main(String[] args) {
+        // byte x = (byte) (200L-200L);
+
+        Variables v = new Variables();
+        v.x = 10;
+        System.out.println(v.x);
+
         // overflow
         byte x = 127;
 
@@ -29,28 +62,7 @@ public class Variables {
             ότι θέλουμε τον πιο *ακριβή* τύπο, δηλαδή double.
             Αν θέλουμε float, πρέπει να το δηλώσουμε.
          */
-        precisionLoss();
-
-    }
-
-    static void precisionLoss(){
-        // Ο αριθμός 16.777.217 χρειάζεται 25 bits για να αναπαρασταθεί πλήρως
-        int originalInt = 16777217;
-
-        // Αυτόματη μετατροπή (widening primitive conversion) από int σε float
-        float promotedFloat = originalInt;
-
-        // Μετατροπή ξανά σε int για να δούμε αν χάθηκε κάτι
-        int backToInt = (int) promotedFloat;
-
-        System.out.println("Αρχικός Int:  " + originalInt);
-        System.out.println("Τιμή σε Float: " + promotedFloat);
-        System.out.println("Int μετά τη μετατροπή: " + backToInt);
-
-        if (originalInt != backToInt) {
-            System.out.println("\nΠΡΟΣΟΧΗ: Οι τιμές ΔΕΝ είναι ίσες!");
-            System.out.println("Διαφορά: " + (originalInt - backToInt));
-        }
+        v.precisionLoss();
 
     }
 
